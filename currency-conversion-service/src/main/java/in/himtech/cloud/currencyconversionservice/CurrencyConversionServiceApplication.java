d30da58506c59c08cee9a25e65b37699a4f324da
hupadhyay <himanshu2703@gmail.com>
2018-07-14 17:32:19 -0400

    eureka server implemented

--- currency-conversion-service/src/main/java/in/himtech/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/in/himtech/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package in.himtech.cloud.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages="in.himtech.cloud.currencyconversionservice.client")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
